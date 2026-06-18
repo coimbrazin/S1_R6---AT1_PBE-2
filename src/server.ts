1293198d9b48f159dd628f6bbb5d3de2ae5d45fb
coimbrazin <coimbraccmxd@hotmail.com>
2026-06-18 10:17:13 -0300

    Modificando server e package.json

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,16 @@ import router from './routes/routes';
 import { initializeDatabase } from './database/connection.database';
 
 const app = express();
+
 app.use(express.json());
 app.use('/', router);
 
-initializeDatabase().then(() => {
-    app.listen(process.env.SERVER_PORT, () => {
-        console.log(`Servidor rodando na porta ${process.env.SERVER_PORT}`);
+initializeDatabase()
+    .then(() => {
+        app.listen(EnvVar.SERVER_PORT, () => {
+            console.log(`Servidor rodando na porta ${EnvVar.SERVER_PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error('Erro ao inicializar o banco de dados:', err);
     });
-}).catch(err => {
-    console.error("Erro ao inicializar o banco de dados:", err);
-});
